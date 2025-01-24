2ac63b729149d9ad80d600087f27b17cb339eea4
JACK\tele8 <tele81427@gmail.com>
2025-01-25 07:49:52 +0800

    update dbconnectionstring setting

--- Base.Core/DbSetting/DbSettings.cs
+++ Base.Core/DbSetting/DbSettings.cs
@@ -1,9 +1,15 @@
 ﻿using Base.Core.DbSetting;
+using Microsoft.Extensions.Configuration;
 
 namespace Base.Core
 {
     public class DbSettings : IDbSettings
     {
-        public string ConfigTemplate { get; set; } = "";
+        public string ConfigTemplate { get; set; }
+
+        public DbSettings(IConfiguration configuration)
+        {
+            ConfigTemplate = configuration.GetConnectionString("ShoppingMartConnection");
+        }
     }
 }
